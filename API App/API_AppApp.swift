271da18b2e69607d9fd3f7046dfbd8bcd424f9e3
Quinn Kozak <quinn.kozak@icloud.com>
2023-02-22 09:41:52 -0600

    Initial Commit

---
+++ API App/API_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_AppApp.swift
+//  API App
+//
+//  Created by Quinn Kozak on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct API_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
